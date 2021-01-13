bc2d5f1fa7097eee357e77ef5682ace35be95046
CHBoris111 <76848107+CHBoris111@users.noreply.github.com>
2021-01-13 21:16:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,77 @@
-#kek
+'''
+Подружить со вторым циклом
+import os
+import sys
+import pygame
+
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{name}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Monster(pygame.sprite.Sprite):
+    image = load_image("mountains.png")
+
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = Monster.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.bottom = height
+
+
+class Food(pygame.sprite.Sprite):
+    image = load_image("pt.png")
+
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = Food.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        if not pygame.sprite.collide_mask(self, mountain):
+            self.rect = self.rect.move(1, 1)
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Шарики')
+    all_sprites = pygame.sprite.Group()
+    mountain = Monster()
+
+    fps = 60
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                Food(event.pos)
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((255, 255, 255))
+        all_sprites.draw(screen)
+        all_sprites.update()
+        clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()
+'''
 import os
 import pygame
 import pygame_gui
